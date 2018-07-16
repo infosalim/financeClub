384969859c3efdfd1377071cc90621bbc6657cb8
Md Salim Hossain <salimh5757@gmail.com>
2018-07-17 00:12:16 +0600

    I have worked

--- assets/js/active.js
+++ assets/js/active.js
@@ -1,7 +1,19 @@
+(function ($) {
+  "use strict";
+  
+  jQuery(document).ready(function($){
+    startAnimation();
+    function startAnimation(){
+      jQuery('.skill-bar').each(function(){
+  
+        jQuery(this).find('.skillbar').animate({
+          width:jQuery(this).attr('data-percent')
+        },6000); 
+      });
+    }
 
-$(document).ready(function(){
-
-
-  // wow js activate
-  new WOW().init();
-});
+      
+  });
+  
+ 
+}(jQuery));
